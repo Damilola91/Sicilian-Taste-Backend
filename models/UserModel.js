126badbcd73bfc8cd979cc0e5b3bd7f9d9fd14e6
Damilola91 <damilola.wiwoloku@gmail.com>
2024-11-19 20:35:38 +0100

    usermodel

---
+++ models/UserModel.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const allowedRoles = ["user", "admin"];
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+      trim: true,
+    },
+
+    surname: {
+      type: String,
+      required: true,
+      minLength: 3,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+
+    address: {
+      type: String,
+      required: false,
+    },
+
+    role: {
+      type: String,
+      enum: allowedRoles,
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+    strict: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model("userModel", UserSchema, "users");
